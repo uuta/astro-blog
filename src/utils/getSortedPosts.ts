ed65348c5b89b2ea0ea7ee8a2d373d27a1fa0fd4
uuta <nightrage.metal66@gmail.com>
2026-01-04 00:32:10 +0900

    Refactor post type in utils functions

--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -1,23 +1,11 @@
-import type { CollectionEntry } from "astro:content";
+import type { UnifiedPost } from "../types/post";
 
-const getDate = (s: string) => {
-  return s.slice(0, 8);
-};
-
-const getSortedPosts = (posts: CollectionEntry<"blog">[]) =>
+const getSortedPosts = (posts: UnifiedPost[]): UnifiedPost[] =>
   posts
-    .filter(({ data, slug }) => {
-      if (data.draft) {
-        return false;
-      }
-      const re = new RegExp(/^([0-9]|-)*/);
-      const date = re.exec(slug);
-      return date ? !isNaN(new Date(date[0]).getTime()) : false;
-    })
+    .filter(post => !post.data.draft)
     .sort(
       (a, b) =>
-        Math.floor(new Date(getDate(b.slug)).getTime() / 1000) -
-        Math.floor(new Date(getDate(a.slug)).getTime() / 1000)
+        b.data.pubDatetime.getTime() - a.data.pubDatetime.getTime()
     );
 
-export default getSortedPosts;
+export { getSortedPosts };
